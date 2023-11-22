d28f597b705a1faf391487cac89331f26bd60f91
Rafael Felipe da Silva <995870@clamed.com.br>
2023-11-22 13:35:48 -0300

    termino do cap 4.6

--- cap4/4_6_busca_remocao_insercao.c
+++ cap4/4_6_busca_remocao_insercao.c
@@ -17,6 +17,16 @@ celula *nova (int init_conteudo, celula *next) {
     return new;
 }
 
+void print_celulas(celula *primaria){
+    printf("[ ");
+    celula *loop = primaria;
+    while (loop != NULL) {
+        printf("%d ", loop -> conteudo);
+        loop = loop -> seg;
+    }
+    printf("]");
+}
+
 /* Esta função recebe uma lista encadeada primaria com cabeça
 e remove da lista a primeira celula que contiver x, se tal
 celula existir*/
@@ -76,19 +86,132 @@ void buscaRemoveSemCabeca(int x, celula *primaria){
 
 // 4.6.2 Escreva uma versão da função buscaInsere para 
 // listas encadeadas sem cabeça 
-buscaInsereSemCabeca(int y, int x, celula *primaria){
-    // wip
+celula *buscaInsereSemCabeca(int y, int x, celula *primaria){
+    celula *nova;
+    nova = malloc(sizeof(celula));
+    nova -> conteudo = y;
+    if (primaria -> conteudo == x){
+        nova -> seg = primaria;
+        return nova;
+    } else {
+        celula *anterior = primaria;
+        // busca
+        while (anterior -> seg != NULL && anterior -> seg -> conteudo != x){
+            anterior = anterior -> seg;
+        }
+        nova -> seg = anterior -> seg;
+        anterior -> seg = nova;
+        return primaria;
+    }
+}
+
+// 4.6.3 Escreva uma função para remover de uma lista encadeada todos 
+// os elementos que contêm x. Faça uma versão iterativa e uma versão recursiva
+celula *busca(int x, celula *primaria){
+    celula *anterior = primaria;
+    while (anterior -> seg != NULL && anterior -> seg -> conteudo != x){
+        anterior = anterior -> seg;
+    }
+    return anterior;
+}
+
+// iterativa
+void buscaRemoveTodas(int x, celula *primaria){
+    celula *anterior = busca(x, primaria);
+    while (anterior -> seg != NULL && anterior -> seg -> conteudo == x){
+        celula *remover = anterior -> seg;
+        anterior -> seg = remover -> seg;
+        free (remover);
+        anterior = busca(x, primaria);
+    }
+}
+
+// recursiva
+void buscaRemoveTodasR(int x, celula *primaria){
+    celula *anterior = busca(x, primaria);
+    if (anterior -> seg != NULL){
+        if (anterior -> seg -> conteudo == x) {
+            celula *remover = anterior -> seg;
+            anterior -> seg = remover -> seg;
+            free (remover);
+        }
+        buscaRemoveTodasR(x, primaria);
+    }
+}
+
+// 4.6.4 Escreva uma função que remova de uma lista
+// encadeada uma célula cujo conteúdo tem valor mínimo
+// Faça uma versão iterativa e uma recursiva
+
+// iterativa
+celula *minimo(celula *primaria){
+    celula *anterior = primaria, *loop = primaria -> seg;
+    while (loop -> seg != NULL){
+        if (loop -> seg -> conteudo < anterior -> seg -> conteudo)
+            anterior = loop;
+        loop = loop -> seg;
+    }
+    return anterior;
+}
+
+// recursiva
+celula *minimoR(celula *menor, celula *primaria){
+    celula *menor_anterior;
+    if (primaria -> seg != NULL){
+        menor_anterior = minimoR(menor, primaria -> seg);
+        if (menor_anterior -> seg -> conteudo < menor -> seg -> conteudo)
+            menor = menor_anterior;
+    }
+    return menor;
+}
+
+void removeMinimo(celula *primaria){
+    celula *anterior = minimo(primaria);
+    celula *remover = anterior -> seg;
+    anterior -> seg = remover -> seg;
+    free (remover);
 }
 
 int main() {
+    // criando lista encadeada
     celula *p4 = nova(15, NULL);
     celula *p3 = nova(12, p4);
     celula *p2 = nova(13, p3);
     celula *p = nova(11, p2);
+
+    // testando remocao
     buscaRemoveSemCabeca(11, p);
     assert(p -> conteudo == 13);
     buscaRemoveSemCabeca(15, p);
     assert(p -> seg -> conteudo == 12);
     assert(p -> seg -> seg == 0); // 0 <=> NULL
+
+    // testando insercao
+    p = buscaInsereSemCabeca(17, 13, p);
+    assert(p -> conteudo == 17);
+    assert(p -> seg -> conteudo == 13);
+    p = buscaInsereSemCabeca(13, 13, p);
+    p = buscaInsereSemCabeca(15, 13, p);
+
+    // remocao completa iterativa e recursiva
+    buscaRemoveTodas(13, p);
+    celula *find = busca(13, p);
+    assert(find -> seg == 0); // NULL
+    buscaRemoveTodasR(15, p);
+    find = busca(15, p);
+    assert(find -> seg == 0); // NULL
+
+    // encontrar celula anterior à celula
+    // cujo valor é valor minimo
+    find = minimo(p);
+    assert(find -> seg -> conteudo == 12);
+    find = minimoR(p, p);
+    assert(find -> seg -> conteudo == 12);
+
+    // remocao
+    removeMinimo(p);
+    assert(p -> seg == 0);
+    assert(p -> conteudo == 17);
+
     printf("OK");
 }
