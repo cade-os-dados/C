ed75a814046d8694e75aaa49b0a0c69c4cb9f848
Rafael Felipe da Silva <995870@clamed.com.br>
2024-01-19 17:05:48 -0300

    feat: completado o exercicio 5.4.1

--- cap5/5_4.c
+++ cap5/5_4.c
@@ -8,6 +8,8 @@ typedef struct cel {
     struct cel *seg;
 } celula;
 
+// implementação em lista sem cabeça
+// dada pelo livro
 int Remove (celula **es, celula **et){
     celula *p;
     int x;
@@ -41,6 +43,41 @@ void printFila(celula *es, celula *et){
     printf(">");
 }
 
+// 5.4.1 Implemente uma fila em uma lista encadeada com célula-cabeça
+celula *find_t(celula *fila){
+    celula *x = fila;
+    celula *loop = fila;
+    while (loop -> seg != x && loop -> seg != NULL){
+        loop = loop -> seg;
+    }
+    return loop;
+}
+
+void push(celula *fila, int y){
+    celula *nova = malloc(sizeof(celula));
+    celula *t = find_t(fila);
+    t -> seg = nova;
+    nova -> seg = fila;
+    nova -> valor = y;
+}
+
+void pop(celula *fila){
+    celula *s = fila -> seg;
+    fila -> seg = s -> seg;
+    free(s);
+}
+
+void printFilaSemCabeca(celula *fila){
+    celula *loop = fila -> seg;
+    printf("< ");
+    while (loop != fila){
+        printf("%d ", loop->valor);
+        loop = loop -> seg;
+    }
+    printf(">");
+}
+
+
 int main() {
     celula *s, *t;
     s = t = NULL;
@@ -51,4 +88,12 @@ int main() {
     printFila(s, t);
     // printf("%d\n", s->valor);
     // printf("%d\n", s->seg->valor);
+
+    celula *fila = malloc(sizeof(celula));
+    fila -> seg = NULL;
+    push(fila, 12);
+    push(fila, 15);
+    printFilaSemCabeca(fila);
+    pop(fila);
+    printFilaSemCabeca(fila);
 }
