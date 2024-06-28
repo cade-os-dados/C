8af3c8863551a9efc2f626a6ce77fd1751882fbb
Rafael Felipe da Silva <995870@clamed.com.br>
2024-06-28 17:36:36 -0300

    feat: finalizada secao 11.3

---
+++ cap11/11_3.c
@@ -0,0 +1,78 @@
+# include "vec.h"
+# include <stdio.h>
+
+/* Recebe um vetor v[p..r] com p <= r. Rearranja os
+* elementos do vetor e devolve j em p..r tal que
+* v[p..j-1] <= v[j] < v[j+1..r] */
+int separa(int p, int r, int v[])
+{
+    int c, j, k, t;
+    c = v[r]; j = p;
+    for (k = p; k < r; k++)
+        if (v[k] <= c)
+        {
+            t = v[j], v[j] = v[k], v[k] = t;
+            j++;
+        }
+    v[r] = v[j], v[j] = c;
+    return j;
+}
+
+/* Esta função rearranja o vetor v[p..r] com p <= r+1,
+* de modo que ele fique em ordem crescente */
+void quicksort (int p, int r, int v[])
+{
+    int j;
+    if (p < r) {
+        j = separa(p, r, v);
+        quicksort(p, j-1, v);
+        quicksort(j+1, r, v); 
+    }
+}
+
+/* 11.3.6 TAIL RECURSION. Moestre que a segunda invocação da função quicksort
+* pode ser eliminada se trocarmos o if por um while apropriado */
+void tailquicksort(int p, int r, int v[])
+{
+    while (p < r)
+    {
+        int j = separa(p, r, v);
+        tailquicksort(p, j-1, v);
+        p = j+1;
+    }  
+}
+
+/* 11.3.11 VERSÃO ITERATIVA. Escreva uma versão não recursiva
+* do algoritmo quicksort */
+void iterquicksort(int v[], int n)
+{
+    int p = 0; int r = n;
+    while (p < r)
+    {
+        int j = separa(p,r,v);
+        p = j+1;
+    }
+
+    p = 0; r = n;
+    while (p < r)
+    {
+        int j = separa(p,r,v);
+        r = j-1;
+    }
+}
+
+int main(void)
+{
+    int vetor[5] = {14, 5, -5, 18, 20};
+    int vetor2[5] = {14, 5, -5, 18, 20};
+    int vetor3[12] = {14, 5, -5, 18, 20, 74, -15, 24, 40, 0, 3, -44};
+    int expected[5] = {-5, 5, 14, 18, 20};
+    int expected2[12] = {-44, -15, -5, 0, 3, 5, 14, 18, 20, 24, 40, 74};
+    quicksort(0, 4, vetor);
+    tailquicksort(0, 4, vetor2);
+    iterquicksort(vetor3, 11);
+    assert_vec(vetor, expected, 0, 5);
+    assert_vec(vetor2, expected, 0, 5);
+    assert_vec(vetor3, expected2, 0, 12);
+    printf("OK");
+}
