e93ca236204bee0d9658f6963f1299122e7e7048
Rafael Felipe da Silva <995870@clamed.com.br>
2024-08-21 15:48:28 -0300

    remove: comentarios rascunho

--- cap12/include/particoes.h
+++ cap12/include/particoes.h
@@ -28,35 +28,3 @@ void printsub(SubconjuntoHead* head);
 void appendsub(Subconjunto* tail, SubconjuntoHead* l2);
 /*go to the tail*/
 Subconjunto* tailsub(Subconjunto* nottail);
-
-/* tamanhos de subconjuntos -> exemplo
-    1 1 1
-    1 2
-    3
-
-    1 1 1 1
-    1 1 2
-    2 2
-    1 3
-    4
-
-    5
-    4,1
-    3,2
-    3,1,1
-    2,2,1
-    2,1,1,1
-    1,1,1,1,1
-
-    6
-    5 1
-    4 2
-    4 1 1
-    3 3
-    3 2 1
-    3 1 1 1
-    2 2 2
-    2 2 1 1
-    2 1 1 1 1
-    1 1 1 1 1 1
-*/
